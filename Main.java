497c5779425845e482fc24de6382c6ec389a669b
vldorl <vldorl@yandex.ru>
2016-05-18 22:53:15 +0300

    Added comments

--- Main.java
+++ Main.java
@@ -1,5 +1,3 @@
-
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.Reader;
@@ -11,6 +9,7 @@ import net.contentobjects.jnotify.linux.JNotify_linux;
 
 public class Main {
 		
+		//главный метод программы, точка входа
 		public static void main(String[] args) throws Exception{
 		
 		System.out.println("Input directory name (from " + System.getProperty("user.home")+ "/ )");//для ввода просматриваемой директории
